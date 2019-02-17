5d2840169883e60f0fa98dc97f90c33ea2739159
capcup <cemcap10@gmail.com>
2019-02-17 12:43:09 +0100

    Some Changes

--- ai.pl
+++ ai.pl
@@ -1,19 +1,45 @@
+team(fc_barcelona, spain).
+team(real_madrid, spain).
+team(bayern_muenchen, germany).
+team(schalke_04, germany).
+
+player(messi).
+player(iniesta).
+player(benzema).
+player(lewandowski).
+
+info(fc_barcelona, [messi,suarez], test).
+info(fc_barcelona, iniesta,test).
+
+team_info(X, Y):-
+    team(X, Y),
+    format('Team ~w is from ~w', [X,Y]),
+    nl,
+    fail.
+team_info(_).
+
+
+print_info(X):-
+    info(X, Y,_),
+    write('Player: '),
+	write(Y),
+    nl,
+    fail.
+print_info(_).
+
+
+
 start:- 
-    write('Hello, what is your name? '),
+    write('Hello, what is your name? (in lower case) '),
     read(Name),
     nl,
     asserta(name(Name)),
-    get_names(Name),
+    format('Hello ~w', Name),
     nl.
 
 
 :- dynamic(name/1).
 
-
-get_names(Name):-
-    name(Name),
-    format('Hello ~w', Name).
-    
 
 undo :- 
 	retract(name(_)), 
